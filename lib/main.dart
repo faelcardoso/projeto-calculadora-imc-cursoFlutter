06b0f167d6f7d275d7954b9fefecb553c0eff316
faelcardoso <faelcjeronymo@gmail.com>
2021-07-08 15:19:44 -0300

    +Botão de cálculo, +Insira os seus dados

--- lib/main.dart
+++ lib/main.dart
@@ -22,128 +22,111 @@ class _HomeState extends State<Home> {
             title: Text("Calculadora IMC"),
             centerTitle: true,
             backgroundColor: Color(0xFF0000535),
-            actions: <Widget>[
-              IconButton(
+            actions: <Widget>[ IconButton(
                   icon: Icon(Icons.refresh),
                   onPressed: () {
                     debugPrint("Funcionou!");
-                  })
+                  }),
             ]),
         backgroundColor: Color(0xFFEAEAEA),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        body: Column( 
+          mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.person,
+              Center(
+                child: Icon(Icons.person,
                     size: 150.0,
-                    color: Color(0xFF0000535),
-                  ),
-                ],
-                mainAxisAlignment: MainAxisAlignment.center,
+                    color: Color(0xFF0000535),),),
+              Center(
+                child: Text("Informe os seus Dados:"),
               ),
-              Padding(
-                padding: 
-                  EdgeInsets.only(
+              Center( // First Text Field
+                child: Padding(
+                  padding: EdgeInsets.only(
                     left: 40.0,
                     right: 40.0,
                     top: 20.0,
-                    bottom: 35.0,
-                  ),
-                child: 
-                  Form(
-                    child: 
-                      TextFormField(
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.numberWithOptions(
-                          signed: true
-                        ),
-                        decoration: 
-                          InputDecoration(
-                            floatingLabelBehavior: FloatingLabelBehavior.never,
-                            labelStyle: 
-                              TextStyle(
-                                color: 
-                                  Color(0xFF0000535),
-                                fontSize: 
-                                  18, 
-                              ),
-                            labelText: 
-                              "Peso (Kg)",
+                    bottom: 35.0,),
+                  child: TextFormField(
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.numberWithOptions(
+                            signed: true),
+                          decoration: InputDecoration(
+                            labelStyle: TextStyle(
+                                color: Color(0xFF0000535),
+                                fontSize: 18),
+                            labelText: "Peso (Kg)",
                             hintText: "Digite o seu peso em quilogramas", 
                             hintStyle: TextStyle(
-                              color: Color(0xFF0000535),
-                            ),
-                            enabledBorder: 
-                              OutlineInputBorder(
+                                color: Color(0xFF0000535),
+                                fontSize: 18),
+                            enabledBorder: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(25.0),
                                 borderSide: BorderSide(color: Color(0xFF0000535),
-                                width: 4.0,)
+                                width: 2.0,)
                               ),
-                            focusedBorder: 
-                              OutlineInputBorder(
-                                borderSide:
-                                  BorderSide(
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
                                     color: Color(0xFF0000535),
                                     width: 4.0,
                                   ),
-                                borderRadius: BorderRadius.circular(25.0),)
-                        ),
-                        style:
-                          TextStyle(
-                            color: Color(0xFFEAEAEA),
-                          ),
-                      ),),
+                                borderRadius: BorderRadius.circular(25.0),),),
+                        style: TextStyle(
+                            color: Color(0xFF0000535),),
+                  ),
                 ),
-                Padding(
-                padding: 
-                  EdgeInsets.only(
+              ),
+              Center( // Second Text Field
+                child: Padding(
+                padding: EdgeInsets.only(
                     left: 40.0,
-                    right: 40.0,
-                    ),
-                child: 
-                  Form(
-                    child: 
-                      TextFormField(
+                    right: 40.0,),
+                child: TextFormField(
                         textAlign: TextAlign.center,
-                        keyboardType: TextInputType.numberWithOptions(
-                          signed: true
-                        ),
-                        decoration: 
-                          InputDecoration(
-                            labelText:
-                              "Altura (m)",
-                              labelStyle: 
-                              TextStyle(
+                        keyboardType: TextInputType.numberWithOptions(signed: true),
+                        decoration: InputDecoration(
+                            labelText: "Altura (m)",
+                              labelStyle: TextStyle(
                                 color: Color(0xFF0000535),
-                              ),
+                                fontSize: 18,),
                             hintText: "Digite a sua altura em metros",
-                            hintStyle: TextStyle(
-                              color: Color(0xFF0000535)
-                            ),
-                            fillColor: 
-                              Colors.white,
-                            enabledBorder: 
-                              OutlineInputBorder(
+                              hintStyle: TextStyle(
+                                color: Color(0xFF0000535)),
+                            enabledBorder: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(25.0),
-                                borderSide: BorderSide(color: Color(0xFF0000535))
-                              ),
-                              focusedBorder: 
-                              OutlineInputBorder(
-                                borderSide:
-                                  BorderSide(
-                                    color: Color(0xFF0000535),
-                                  ),
+                                borderSide: BorderSide(color: Color(0xFF0000535), width: 2.0,)),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xFF0000535),),
                                 borderRadius: BorderRadius.circular(25.0),),
-                            focusColor: Color(0xFFFFD290)
-                        ),
-                        style: 
-                          TextStyle(
-                            color: Colors.black,
-                          ),
-                      ),),
-                )
-            ]));
+                            focusColor: Color(0xFFFFD290)),
+                        style: TextStyle(
+                            color: Colors.black,)
+                            ,)
+                         ,)
+                      ,),
+                   Center( // Calculate Button
+                     child: Column(
+                       children: <Widget> [
+                         Padding(
+                           padding: EdgeInsets.all(40.0),
+                           child: ElevatedButton(
+                          child: Text("Calcular", style: TextStyle(color: Color(0xFF0000535), fontSize: 18,),),
+                           onPressed: () { // Calc Function
+                             debugPrint("Opa!");
+                           },
+                           style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Color(0xFFEAEAEA)),
+                            side: MaterialStateProperty.all(BorderSide(
+                              color: Color(0xFF0000535), 
+                              width: 2.0,)),
+                            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            )),
+                           ),
+                           ),),
+                       ],),),
+                   ]
+                ,)
+          );
   }
 }
---
+++ projeto.md
@@ -0,0 +1,30 @@
+# Projeto calculadora IMC
+
+## O que precisa fazer?
+
+### Interface
+
+* Botão para resetar os campos do formulário.
+* Ícone de Pessoa.
+* Dois campos de Texto contendo as seguintes informações:
+    * Digite seu peso(Kg).
+    * Digite sua Altura(m).
+* Um botão para iniciar o cálculo.
+* Texto pedindo para a pessoa informar os seus dados.
+* Permitir somente a entrada de teclado numérico do celular.
+* Verificação do preenchimento dos dados.
+* Design Arredondado (Opcional)
+
+### Algoritmo
+
+* Cálculo = Peso / Altura² (peso / (altura * altura))
+* Puxar as informações preenchidas no formulário.
+* Retornar o resultado e o grau do IMC que a pessoa tem:
+    * 18,5 ou menos: Abaixo do normal
+    * Entre 18,6 e 24,9: Normal
+    * Entre 25 e 29,9: Sobrepeso
+    * Entre 30 e 34,9: Obesidade Grau I
+    * Entre 35 e 39,9: Obesidade Grau II
+    * Acima de 40: Obesidade Grau III
+* Apagar as informações digitadas no campo do formulário.
+
